4aacea0ca76c5034fb25542d26688437a6f8bf0b
CailanStm <cailanstm@shaw.ca>
2017-03-19 15:32:24 -0700

    Create Java project for source code extraction

---
+++ source_code_analyzer/SourceCodeAnalyzer/src/main/java/talspals/source_code_analyzer/SourceCodeAnalyzer/SourceCodeAnalyzer.java
@@ -0,0 +1,9 @@
+package talspals.source_code_analyzer.SourceCodeAnalyzer;
+
+import com.github.javaparser.*;
+
+public class SourceCodeAnalyzer {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
